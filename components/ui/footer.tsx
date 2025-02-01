4ad25ad73a98625ee0293f3c40245180bf4f4869
zxTomw <tom_zhixing@outlook.com>
2025-01-31 20:07:46 -0500

    changed footer

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,10 +6,10 @@ export function Footer() {
   return (
     <footer className="px-16 container mx-auto py-20 flex text-muted-foreground justify-between items-center">
       <div className="">
-        <h3 className=" font-semibold">Tom's portfolio</h3>
-        <p className="text-sm">
+        <p className=" font-semibold">
           Created with NextJS and Shadcn/ui, deployed on Vercel.
         </p>
+        <p className="text-sm">by Tom Wang</p>
         <div className="flex gap-2 pt-1 items-center ">
           <Link href="https://github.com/zxTomw" target="_blank">
             <FontAwesomeIcon

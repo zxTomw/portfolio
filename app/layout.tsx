3e9615737e0a796e2052ee0b765c761e0242c5b6
zxTomw <tom_zhixing@outlook.com>
2025-02-02 21:50:38 -0500

    upgrade to next15, enabled react compiler

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased `}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />

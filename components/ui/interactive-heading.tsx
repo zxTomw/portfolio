c28b029ad69862803ed695b7dd3924f0a305e23e
zxTomw <tom_zhixing@outlook.com>
2025-02-25 13:08:37 -0500

    heading interactivity hint

--- components/ui/interactive-heading.tsx
+++ components/ui/interactive-heading.tsx
@@ -24,6 +24,7 @@ export function InteractiveHeading() {
         {iam}
       </span>
       <span
+        className="cursor-pointer"
         onClick={() =>
           setName((prev) => {
             switch (prev) {

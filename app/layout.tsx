d106fd7a9d1756f8d12c2301d6770f95f770debb
zxTomw <tom.wang3@uwaterloo.ca>
2025-09-20 14:37:01 -0400

    default dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} font-sans antialiased `}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <Navbar />
           <div className="w-dvw min-h-screen">
             {children}
